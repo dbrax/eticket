ef1f4c6fb1aea25f6c4618a95635c6c625cd5e33
dbrax <epmnzava@gmail.com>
2021-03-31 22:58:15 +0300

    added organizer model

---
+++ database/migrations/create_organizer_table.php
@@ -0,0 +1,46 @@
+<?php
+
+
+/**
+ * Author: Emmanuel Paul Mnzava
+ * Twitter: @epmnzava
+ * Github:https://github.com/dbrax/eticket
+ * Email: epmnzava@gmail.com
+ * 
+ */
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organizer', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('userid')->nullable(); //??? in multitenant or distribution
+            $table->string("name");
+            $table->string("timezone")->nullable();
+            $table->string("logo")->nullable();
+            $table->string("url")->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organizer');
+    }
+}
